9b3abbf794cd61a53d5588a2bd1ce873f87591e1
JakeWang <jake@foundrymakes.com>
2023-05-30 14:00:04 -0400

    make interests section scrollable carousel

--- components/ContactInterests.tsx
+++ components/ContactInterests.tsx
@@ -1,3 +1,8 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+
 interface Props {
   interests: String[];
 }
@@ -5,16 +10,23 @@ interface Props {
 export const ContactInterests = ({ interests }: Props) => {
   return (
     <div>
-      <div className="text-base md:text-lg lg:text-xl mb-2">Interests</div>
-      <div className="bg-white bg-opacity-5 w-full p-4 space-x-2 text-base mb-6 rounded-xl">
-        {interests?.map((interest, index) => (
-          <span
-            key={`interest-${index}`}
-            className="inline-block bg-white bg-opacity-[0.12] text-white rounded-2xl px-4 py-[6px] text-sm md:text-base lg:text-lg"
-          >
-            {interest}
-          </span>
-        ))}
+      <div className="text-base md:text-lg lg:text-xl mb-3">Interests</div>
+      <div className="text-base mb-8 rounded-xl">
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={8}
+          freeMode={true}
+          modules={[FreeMode]}
+        >
+          {interests?.map((interest, index) => (
+            <SwiperSlide
+              key={`interest-${index}`}
+              className="!w-auto inline-block bg-white bg-opacity-[0.12] text-white rounded-2xl px-4 py-[6px] text-sm md:text-base lg:text-lg"
+            >
+              {interest}
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
