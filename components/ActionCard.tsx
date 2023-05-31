c3ecab91d9217d562765f20d58ad5e36c80da5dc
JakeWang <jake@foundrymakes.com>
2023-05-31 19:38:17 -0400

    fix overflow

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -13,10 +13,12 @@ export const ActionCard = ({ action, actionType }: Props) => {
       <div className="bg-white bg-opacity-5 p-4 mb-4 hover:bg-opacity-[0.08] active:bg-opacity-10 rounded-lg">
         <div className="flex flex-col">
           <div className="flex justify-between">
-            <h2 className="md:text-xl lg:text-2xl">{action.contactName}</h2>
-            <ArrowRight className="md:w-6 md:h-6 lg:w-8 lg:h-8" />
+            <h2 className="md:text-xl lg:text-2xl overflow-hidden break-words">
+              {action.contactName}
+            </h2>
+            <ArrowRight className="md:w-6 md:h-6 lg:w-8 lg:h-8 flex-shrink-0" />
           </div>
-          <div className="text-sm font-normal text-white text-opacity-70 md:text-base lg:text-lg">
+          <div className="text-sm font-normal text-white text-opacity-70 md:text-base lg:text-lg overflow-hidden break-words">
             {action.contactIndustry && <>{action.contactIndustry} • </>}
             Goal: {action.goalDays} days
           </div>
--- components/ContactCard.tsx
+++ components/ContactCard.tsx
@@ -12,12 +12,12 @@ export const ContactCard = ({ contact }: Props) => {
       <div className="bg-white bg-opacity-5 p-4 mb-4 hover:bg-opacity-[0.08] active:bg-opacity-10 rounded-lg">
         <div className="flex flex-col">
           <div className="flex justify-between">
-            <div className="text-base md:text-xl lg:text-2xl">
+            <div className="text-base md:text-xl lg:text-2xl overflow-hidden break-words">
               {contact.name}
             </div>
-            <ArrowRight className="md:w-6 md:h-6 lg:w-8 lg:h-8" />
+            <ArrowRight className="md:w-6 md:h-6 lg:w-8 lg:h-8 flex-shrink-0" />
           </div>
-          <div className="text-sm font-normal text-white text-opacity-70 mb-2 md:text-base lg:text-lg">
+          <div className="text-sm font-normal text-white text-opacity-70 mb-2 md:text-base lg:text-lg overflow-hidden break-words">
             {`${contact?.industry}${contact?.isArchived ? " • Archived" : ""}`}
           </div>
           <p className="text-sm md:text-base lg:text-lg line-clamp-2 overflow-hidden">
