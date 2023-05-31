143acb2f7b43eaa99b708770f65acd4856898a67
JakeWang <jake@foundrymakes.com>
2023-05-31 11:41:29 -0400

    remove mic icon from search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -12,10 +12,6 @@ export const SearchBar = () => {
         placeholder="Search contact"
         className="flex-grow bg-transparent focus:outline-none text-base md:text-lg lg:text-xl"
       />
-      <Mic
-        size={16}
-        className="mx-2 my-auto text-grey opacity-60 md:w-5 md:h-5 lg:w-6 lg:h-6"
-      />
     </div>
   );
 };
