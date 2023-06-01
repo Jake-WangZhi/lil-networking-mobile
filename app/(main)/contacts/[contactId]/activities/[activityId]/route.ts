d9305a8921a784c3133608f5618a31e0aad44512
JakeWang <jake@foundrymakes.com>
2023-06-01 13:42:58 -0400

    hot fix

--- app/(main)/contacts/[contactId]/activities/[activityId]/route.ts
+++ app/(main)/contacts/[contactId]/activities/[activityId]/route.ts
@@ -1,11 +1,11 @@
-import { Activity } from "@/types";
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
 export async function DELETE(
   request: Request,
   { params }: { params: { activityId: string } }
 ) {
-  const activity = await prisma?.activity.delete({
+  const activity = await prisma.activity.delete({
     where: { id: params.activityId },
   });
 
