01eb8db7aa51592bab9d4aff1972ce697c5a48b0
JakeWang <jake@foundrymakes.com>
2023-06-07 12:43:49 -0400

    hot fix

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -8,8 +8,9 @@ export const SearchBar = () => {
         className="mx-2 my-auto text-grey opacity-60 md:w-5 md:h-5 lg:w-6 lg:h-6"
       />
       <input
+        id="searchBarInput"
         type="text"
-        placeholder="Search contact"
+        placeholder="Search"
         className="flex-grow bg-transparent focus:outline-none text-base md:text-lg lg:text-xl"
       />
     </div>
