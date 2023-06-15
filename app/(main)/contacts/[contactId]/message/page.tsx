3df5b1a51ec9810a8317b8f5e045e3426d4bdbad
JakeWang <jake@foundrymakes.com>
2023-06-15 10:48:38 -0400

    refactor message card amd swipeableActivities

--- app/(main)/contacts/[contactId]/message/page.tsx
+++ app/(main)/contacts/[contactId]/message/page.tsx
@@ -3,18 +3,17 @@
 import { Button } from "@/components/Button";
 import { useBackPath } from "@/contexts/BackPathContext";
 import { useContact } from "@/hooks/useContact";
-import { Typography, Card, CardContent } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
-import { ChevronLeft, Mail, Linkedin, Check, Archive } from "react-feather";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
+import { ChevronLeft, Check, Archive } from "react-feather";
+
 import "../../styles.css";
 import { ContactInterests } from "@/components/ContactInterests";
 import { useContactMutation } from "@/hooks/useContactMutation";
+import { SwipeableActivities } from "@/components/SwipeableActivities";
+import { ClipLoader } from "react-spinners";
+import { MessageCard } from "@/components/MessageCard";
 
 export default function MessagePage({
   params,
@@ -23,25 +22,11 @@ export default function MessagePage({
 }) {
   const router = useRouter();
   const { setBackPath } = useBackPath();
-
   const { contact, isLoading, isError } = useContact({
     id: params.contactId,
   });
 
   const [errorMessage, setErrorMessage] = useState("");
-
-  const handleLinkedInClick = useCallback(() => {
-    const linkedInLink = contact?.links[0];
-
-    window.open(linkedInLink);
-  }, [contact?.links]);
-
-  const handleEmailClick = useCallback(() => {
-    const emailAddress = contact?.email;
-    const mailtoLink = `mailto:${emailAddress}`;
-
-    window.open(mailtoLink);
-  }, [contact?.email]);
 
   const updateContactMutation = useContactMutation({
     method: "PUT",
@@ -62,6 +47,32 @@ export default function MessagePage({
         isArchived: true,
       });
   }, [contact, updateContactMutation]);
+
+  if (isError) {
+    return (
+      <Typography
+        variant="h3"
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "32px",
+          color: "#F42010",
+        }}
+      >
+        Something went wrong, please try again later
+      </Typography>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center pt-8">
+        <ClipLoader color="#38ACE2" size={150} />
+      </div>
+    );
+  }
 
   if (!contact) {
     return (
@@ -85,27 +96,35 @@ export default function MessagePage({
     <main className="relative min-h-screen text-white py-8 flex flex-col justify-between">
       <div>
         <div className="px-4">
-          <div className="flex justify-between items-center">
-            <Button
-              variant="text"
-              onClick={() => {
-                setBackPath("/dashboard");
-                router.push("/dashboard");
-              }}
-              sx={{ py: "6px" }}
+          <Grid container alignItems="center">
+            <Grid item xs={2}>
+              <Button
+                variant="text"
+                onClick={() => {
+                  setBackPath("/dashboard");
+                  router.push("/dashboard");
+                }}
+                sx={{ py: "6px" }}
+              >
+                <ChevronLeft
+                  size={36}
+                  className="md:w-11 md:h-11 lg:w-13 lg:h-13"
+                />
+              </Button>
+            </Grid>
+            <Grid
+              item
+              xs={8}
+              sx={{ display: "flex", justifyContent: "center" }}
             >
-              <ChevronLeft
-                size={36}
-                className="md:w-11 md:h-11 lg:w-13 lg:h-13"
-              />
-            </Button>
-            <Typography
-              variant="h3"
-              sx={{ fontWeight: 600 }}
-            >{`Connect with ${contact?.firstName}`}</Typography>
-            <div></div>
-          </div>
-          <div className="flex justify-center mt-2 mb-5">
+              <Typography
+                variant="h3"
+                sx={{ fontWeight: 600 }}
+              >{`Connect with ${contact?.firstName}`}</Typography>
+            </Grid>
+            <Grid item xs={2}></Grid>
+          </Grid>
+          <div className="flex justify-center mb-5">
             <Button
               variant="text"
               onClick={() => {
@@ -116,96 +135,15 @@ export default function MessagePage({
                 py: "12px",
               }}
             >
-              <Typography variant="subtitle1">View Profile</Typography>
+              <Typography variant="subtitle1" sx={{ opacity: 0.7 }}>
+                View Profile
+              </Typography>
             </Button>
           </div>
-          <Card
-            sx={{
-              backgroundColor: "rgba(255, 255, 255, 0.05)",
-              borderRadius: "8px",
-              boxShadow: "none",
-            }}
-          >
-            <CardContent>
-              <div className="space-y-2">
-                <div className="flex justify-between items-center">
-                  <Typography variant="h2">
-                    {contact.firstName} {contact.lastName}
-                  </Typography>
-                  <div className="flex items-start">
-                    <Button
-                      variant="text"
-                      sx={{ p: "12px" }}
-                      onClick={handleEmailClick}
-                    >
-                      <Mail size={24} />
-                    </Button>
-                    <Button
-                      variant="text"
-                      sx={{ p: "12px" }}
-                      onClick={handleLinkedInClick}
-                    >
-                      <Linkedin size={24} />
-                    </Button>
-                  </div>
-                </div>
-                <Typography variant="subtitle1">{contact.title}</Typography>
-                <Typography variant="subtitle1">{contact.company}</Typography>
-                <Typography variant="subtitle1">{contact.industry}</Typography>
-              </div>
-            </CardContent>
-          </Card>
+          <MessageCard contact={contact} />
         </div>
-        <div className="my-6 space-y-4 ml-4">
-          <Typography variant="subtitle1">Most recent Activity</Typography>
-          <Swiper
-            slidesPerView={"auto"}
-            spaceBetween={16}
-            freeMode={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[FreeMode, Pagination]}
-            className="!pb-10"
-          >
-            {contact.activities.slice(0, 3).map((activity, index) => (
-              <SwiperSlide key={`interest-${index}`}>
-                <Card
-                  sx={{
-                    backgroundColor: "rgba(255, 255, 255, 0.05)",
-                    borderRadius: "8px",
-                    boxShadow: "none",
-                    height: "124px",
-                  }}
-                >
-                  <CardContent>
-                    <div className="flex justify-between">
-                      <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
-                        {activity.title}
-                      </Typography>
-                    </div>
-                    <Typography
-                      variant="body1"
-                      sx={{ opacity: 0.7, marginBottom: "8px" }}
-                    >
-                      {activity.date}
-                    </Typography>
-                    <Typography
-                      variant="body1"
-                      sx={{
-                        overflow: "hidden",
-                        display: "-webkit-box",
-                        WebkitBoxOrient: "vertical",
-                        WebkitLineClamp: 2,
-                      }}
-                    >
-                      {activity.description}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+        <div className="my-6">
+          <SwipeableActivities activities={contact.activities.slice(0, 3)} />
         </div>
         <ContactInterests interests={contact.interests} />
       </div>
---
+++ components/SwipeableActivities.tsx
@@ -0,0 +1,68 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { Activity } from "@/types";
+
+interface Props {
+  activities: Activity[];
+}
+
+export const SwipeableActivities = ({ activities }: Props) => {
+  return (
+    <div className="space-y-4 ml-4">
+      <Typography variant="subtitle1">Most recent Activity</Typography>
+      <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={16}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="!pb-10"
+      >
+        {activities.map((activity, index) => (
+          <SwiperSlide key={`interest-${index}`} className="!w-[342px]">
+            <Card
+              sx={{
+                backgroundColor: "rgba(255, 255, 255, 0.05)",
+                borderRadius: "8px",
+                boxShadow: "none",
+                height: "124px",
+              }}
+            >
+              <CardContent>
+                <div className="flex justify-between">
+                  <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
+                    {activity.title}
+                  </Typography>
+                </div>
+                <Typography
+                  variant="body1"
+                  sx={{ opacity: 0.7, marginBottom: "8px" }}
+                >
+                  {activity.date}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{
+                    overflow: "hidden",
+                    display: "-webkit-box",
+                    WebkitBoxOrient: "vertical",
+                    WebkitLineClamp: 2,
+                  }}
+                >
+                  {activity.description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
