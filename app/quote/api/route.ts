2a68a50d8a16f115ed87ee92f4cf32995352ad77
JakeWang <jake@foundrymakes.com>
2023-06-20 15:51:30 -0400

    remove unused request

--- app/quote/api/route.ts
+++ app/quote/api/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(request: Request) {
+export async function GET() {
   const quotes = await prisma.quote.findMany();
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
