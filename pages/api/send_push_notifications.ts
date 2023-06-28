f478c320a4466b762dc27f4acf365e829c68be32
JakeWang <jake@foundrymakes.com>
2023-06-28 15:39:39 -0400

    testing

--- pages/api/send_push_notifications.ts
+++ pages/api/send_push_notifications.ts
@@ -2,35 +2,30 @@ import { NextApiRequest, NextApiResponse } from "next";
 import webpush from "web-push";
 import prisma from "@/lib/prisma";
 
+webpush.setVapidDetails(
+  process.env.VAPID_MAILTO ?? "",
+  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY ?? "",
+  process.env.VAPID_PRIVATE_KEY ?? ""
+);
+
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
   try {
-    webpush.setVapidDetails(
-      process.env.VAPID_MAILTO ?? "",
-      process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY ?? "",
-      process.env.VAPID_PRIVATE_KEY ?? ""
-    );
-
     const subscriptionData: any[] = await prisma.subscription.findMany();
-    console.log("subscriptionData", subscriptionData);
+
     const notificationData = {
       title: "Push Notification Title",
       body: "This is a push notification",
     };
 
-    const sendNotifications = async () => {
-      for (const data of subscriptionData) {
-        const notification = await webpush.sendNotification(
-          data,
-          JSON.stringify(notificationData)
-        );
-        console.log("notification", notification);
-      }
-    };
-
-    await sendNotifications();
+    for (const { endpoint, keys } of subscriptionData) {
+      await webpush.sendNotification(
+        { endpoint, keys },
+        JSON.stringify(notificationData)
+      );
+    }
 
     response
       .status(200)
