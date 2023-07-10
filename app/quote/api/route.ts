8de07603ce6fc63b7238e035f1bbe41caa4fa2c8
JakeWang <jake@foundrymakes.com>
2023-07-10 16:30:25 -0400

    add a comment

--- app/quote/api/route.ts
+++ app/quote/api/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url); //to make sure Math.random() is revoked on every request
+  //So that the function is not cached to ensure Math.random() is re-evaluated on every request.
+  const { searchParams } = new URL(request.url);
 
   const count = await prisma.quote.count();
   const skip = Math.floor(Math.random() * count);
