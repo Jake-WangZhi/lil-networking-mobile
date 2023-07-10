957fc4227695924dd0fe00c7caa742182e3a11fb
JakeWang <jake@foundrymakes.com>
2023-07-10 16:35:39 -0400

    modify the comment

--- app/quote/api/route.ts
+++ app/quote/api/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
-  //So that the function is not cached to ensure Math.random() is re-evaluated on every request.
+  //Keep this line to prevent the function from being cached
+  //To ensure it returns a different quote randomly with each request.
   const { searchParams } = new URL(request.url);
 
   const count = await prisma.quote.count();
