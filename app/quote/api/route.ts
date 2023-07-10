905d5b65b7dbb53db45ca3c81383849359a63797
JakeWang <jake@foundrymakes.com>
2023-07-10 15:33:58 -0400

    use current timestamp to generate random result

--- app/quote/api/route.ts
+++ app/quote/api/route.ts
@@ -3,7 +3,10 @@ import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
   const quotes = await prisma.quote.findMany();
-  const randomIndex = Math.floor(Math.random() * quotes.length);
+
+  const seed = Date.now(); // Dynamic seed based on current timestamp
+  const randomIndex = Math.floor(seed * quotes.length);
+
   const randomQuote = quotes[randomIndex];
 
   return NextResponse.json(randomQuote);
