ef2ad3f321d65e5b56ada5258ab2da1a8c60408e
Jake Wang <jake@foundrymakes.com>
2023-07-18 17:48:10 -0400

    add ios a2hs instruction

--- components/AddToHomeScreenBanner.tsx
+++ components/AddToHomeScreenBanner.tsx
@@ -1,4 +1,4 @@
-import { X } from "react-feather";
+import { X, Share } from "react-feather";
 import icon from "@/public/icons/icon-512x512.png";
 import Image from "next/image";
 import { Button } from "./Button";
@@ -27,6 +27,7 @@ export const AddToHomeScreenBanner = ({ addBottomPadding }: Props) => {
   const [deferredPrompt, setDeferredPrompt] =
     useState<BeforeInstallPromptEvent | null>(null);
   const [isModalOpened, setIsModalOpened] = useState(false);
+  const [isIOS, setIsIOS] = useState(false);
 
   const handleBeforeInstallPrompt = useCallback((e: Event) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -46,6 +47,18 @@ export const AddToHomeScreenBanner = ({ addBottomPadding }: Props) => {
       );
     };
   }, [handleBeforeInstallPrompt]);
+
+  useEffect(() => {
+    // Check if window is defined (to avoid errors during server-side rendering)
+    if (typeof window !== "undefined") {
+      const userAgent = window.navigator.userAgent;
+      const isIOSDevice = /iPhone/.test(userAgent) || /iPad/.test(userAgent);
+      if (isIOSDevice) {
+        setIsIOS(true);
+        setIsModalOpened(true);
+      }
+    }
+  }, []);
 
   const handleA2HSClick = useCallback(() => {
     if (!deferredPrompt) return;
@@ -70,25 +83,48 @@ export const AddToHomeScreenBanner = ({ addBottomPadding }: Props) => {
         >
           <div className="flex items-center space-x-2">
             <Button variant="text" onClick={handleClose}>
-              <X size={24} />
+              <X
+                size={24}
+                className="md:w-7 md:h-7 lg:w-8 lg:h-8 flex-shrink-0"
+              />
             </Button>
             <Image src={icon} height={48} width={48} alt={"icon"} />
+
             <div className="pl-2">
-              <Typography variant="body1" sx={{ fontWeight: 600 }}>
-                The Little Networking App
-              </Typography>
-              <Typography variant="subtitle2" sx={{ color: "white" }}>
-                Add to Home Screen
-              </Typography>
+              {isIOS ? (
+                <Typography variant="body1">
+                  Install this web app on your iPhone or iPad for the best
+                  experience.
+                  <span className="flex items-center space-x-1">
+                    <span>Tap</span>
+                    <Share
+                      size={14}
+                      className="md:w-4 md:h-4 lg:w-5 lg:h-5 flex-shrink-0"
+                    />
+                    <span>and then "Add to Home Screen".</span>
+                  </span>
+                </Typography>
+              ) : (
+                <>
+                  <Typography variant="body1" sx={{ fontWeight: 600 }}>
+                    The Little Networking App
+                  </Typography>
+                  <Typography variant="subtitle2" sx={{ color: "white" }}>
+                    Add to Home Screen
+                  </Typography>
+                </>
+              )}
             </div>
           </div>
-          <Button
-            variant="text"
-            onClick={handleA2HSClick}
-            sx={{ color: "#38ACE2" }}
-          >
-            Install
-          </Button>
+          {!isIOS && (
+            <Button
+              variant="text"
+              onClick={handleA2HSClick}
+              sx={{ color: "#38ACE2" }}
+            >
+              Install
+            </Button>
+          )}
         </div>
       )}
     </>
