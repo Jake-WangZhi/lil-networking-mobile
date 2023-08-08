78b1977ef6e89d68a7cd705fe67df99b855096a2
JakeWang <jake@foundrymakes.com>
2023-08-08 15:52:57 -0400

    modify the User table model and remove clerk endpoint

--- apps/web/prisma/migrations/20230808193845_modify_tables_for_clerk_auth/migration.sql
+++ apps/web/prisma/migrations/20230808193845_modify_tables_for_clerk_auth/migration.sql
@@ -1,15 +1,12 @@
 /*
   Warnings:
 
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
   - You are about to drop the column `emailVerified` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `image` on the `User` table. All the data in the column will be lost.
   - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
   - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
   - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
-  - A unique constraint covering the columns `[phone]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[externalId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `externalId` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -27,25 +24,20 @@ ALTER TABLE "NotificationSettings" DROP CONSTRAINT "NotificationSettings_subscri
 -- DropForeignKey
 ALTER TABLE "Subscription" DROP CONSTRAINT "Subscription_userId_fkey";
 
+-- DropIndex
+DROP INDEX "User_email_key";
+
 -- AlterTable
-ALTER TABLE "User" DROP COLUMN "emailVerified",
-DROP COLUMN "name",
-ADD COLUMN     "externalId" TEXT NOT NULL,
-ADD COLUMN     "firstName" TEXT NOT NULL,
-ADD COLUMN     "lastName" TEXT NOT NULL,
-ADD COLUMN     "phone" TEXT;
+ALTER TABLE "User" DROP COLUMN "email",
+DROP COLUMN "emailVerified",
+DROP COLUMN "image",
+DROP COLUMN "name";
 
 -- DropTable
 DROP TABLE "Account";
 
 -- DropTable
 DROP TABLE "VerificationToken";
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_externalId_key" ON "User"("externalId");
 
 -- AddForeignKey
 ALTER TABLE "Contact" ADD CONSTRAINT "Contact_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
