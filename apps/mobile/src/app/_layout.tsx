b74ac442f98c4d7ba9d8df4308c2a45cf8ff5310
JakeWang <jake@foundrymakes.com>
2023-08-08 17:19:15 -0400

    updates

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -3,11 +3,9 @@ import "@fontsource/metropolis";
 
 import React from "react";
 import { Stack } from "expo-router";
-import { GluestackUIProvider } from "../components/GluestackUIProvider";
-
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { StatusBar } from "react-native";
-import { config } from "gluestack-ui/build/gluestack-ui.config";
+import { GluestackUIProvider, config } from "gluestack-ui";
 
 const RootLayout = () => {
   const CLERK_PUBLISHABLE_KEY =
