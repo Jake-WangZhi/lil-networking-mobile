d1c70e4474edf2c2750954e918936909c9dc9bad
JakeWang <jake@foundrymakes.com>
2023-08-11 08:45:05 -0400

    style the botton tab bar

--- apps/mobile/src/app/main/index.tsx
+++ apps/mobile/src/app/main/index.tsx
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Dashboard from "../dashboard";
-import { Info } from "phosphor-react-native";
+import { House, Users, Gear } from "phosphor-react-native";
 import Contacts from "../contacts";
 import Settings from "../settings";
 
@@ -8,32 +8,63 @@ const Tab = createBottomTabNavigator();
 
 export default function Main() {
   return (
-    <Tab.Navigator sceneContainerStyle={{ backgroundColor: "#0F1A24" }}>
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: "#0F1A24",
+      }}
+    >
       <Tab.Screen
         name="dashboard/index"
         component={Dashboard}
         options={{
-          tabBarIcon: () => <Info />,
+          tabBarIcon: ({ focused }) => (
+            <House
+              color={focused ? "white" : "rgba(255, 255, 255, 0.7)"}
+              weight={focused ? "fill" : "bold"}
+            />
+          ),
           headerShown: false,
           tabBarLabel: "Dashboard",
+          tabBarStyle: { backgroundColor: "#0F1A24", borderTopWidth: 0 },
+          tabBarActiveTintColor: "white",
+          tabBarInactiveTintColor: "rgba(255, 255, 255, 0.7)",
+          tabBarLabelStyle: { fontWeight: "600", fontSize: 11 },
         }}
       />
       <Tab.Screen
         name="contacts/index"
         component={Contacts}
         options={{
-          tabBarIcon: () => <Info />,
+          tabBarIcon: ({ focused }) => (
+            <Users
+              color={focused ? "white" : "rgba(255, 255, 255, 0.7)"}
+              weight={focused ? "fill" : "bold"}
+            />
+          ),
           headerShown: false,
           tabBarLabel: "Contacts",
+          tabBarStyle: { backgroundColor: "#0F1A24", borderTopWidth: 0 },
+          tabBarActiveTintColor: "white",
+          tabBarInactiveTintColor: "rgba(255, 255, 255, 0.7)",
+          tabBarLabelStyle: { fontWeight: "600", fontSize: 11 },
         }}
       />
       <Tab.Screen
         name="settings/index"
         component={Settings}
         options={{
-          tabBarIcon: () => <Info />,
+          tabBarIcon: ({ focused }) => (
+            <Gear
+              color={focused ? "white" : "rgba(255, 255, 255, 0.7)"}
+              weight={focused ? "fill" : "bold"}
+            />
+          ),
           headerShown: false,
-          tabBarLabel: "Contacts",
+          tabBarLabel: "Settings",
+          tabBarStyle: { backgroundColor: "#0F1A24", borderTopWidth: 0 },
+          tabBarActiveTintColor: "white",
+          tabBarInactiveTintColor: "rgba(255, 255, 255, 0.7)",
+          tabBarLabelStyle: { fontWeight: "600", fontSize: 11 },
         }}
       />
     </Tab.Navigator>
--- apps/mobile/src/app/settings/index.tsx
+++ apps/mobile/src/app/settings/index.tsx
@@ -1,7 +1,6 @@
 import { useClerk } from "@clerk/clerk-expo";
-import { Text } from "gluestack-ui";
+import { Button, Text } from "gluestack-ui";
 import { View } from "react-native";
-import { Link } from "expo-router";
 
 export default function Settings() {
   const { signOut } = useClerk();
@@ -9,15 +8,14 @@ export default function Settings() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-white">Hello, this is the settings</Text>
-      <Link
+      <Button
         className="flex items-center justify-center bg-transparent"
         onPress={() => {
           signOut();
         }}
-        href="/login"
       >
         <Text className="text-white">Sign out</Text>
-      </Link>
+      </Button>
     </View>
   );
 }
