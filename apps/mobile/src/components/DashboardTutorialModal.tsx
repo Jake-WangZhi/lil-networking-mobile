d966de82685489701239e26c6eda738c1743ca23
JakeWang <jake@foundrymakes.com>
2023-08-17 17:05:54 -0400

    update import image type

--- apps/mobile/src/components/DashboardTutorialModal.tsx
+++ apps/mobile/src/components/DashboardTutorialModal.tsx
@@ -10,20 +10,20 @@ const data = [
     title: "Add Contacts",
     description:
       "Create new or easily import contacts from your phone to get started.",
-    image: img1 as number,
+    image: img1,
   },
   {
     id: "2",
     title: "Networking Goals",
     description: "Set monthly goals to build and sustain your network.",
-    image: img2 as number,
+    image: img2,
   },
   {
     id: "3",
     title: "Contact Reminders",
     description:
       "Automated and priority sorted reminders appear based on individual cadence.",
-    image: img3 as number,
+    image: img3,
   },
 ];
 
--- apps/mobile/src/types.ts
+++ apps/mobile/src/types.ts
@@ -1,6 +1,8 @@
+import type { ImageSourcePropType } from "react-native";
+
 export interface TutorialModalProps {
   id: string;
   title: string;
   description: string;
-  image: number;
+  image: ImageSourcePropType;
 }
