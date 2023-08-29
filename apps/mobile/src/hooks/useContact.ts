29779f14b0ec78f88ad04a2012114dc3a4c5fe73
JakeWang <jake@foundrymakes.com>
2023-08-29 14:19:09 -0400

    move schema to shared packages

--- apps/mobile/src/hooks/useContact.ts
+++ apps/mobile/src/hooks/useContact.ts
@@ -1,21 +1,7 @@
 import { useAuth } from "@clerk/clerk-expo";
+import { contactSchema } from "@foundrymakes/validation";
 import { useQuery } from "@tanstack/react-query";
-import { z } from "zod";
-
-const contactSchema = z.object({
-  id: z.string(),
-  firstName: z.string(),
-  lastName: z.string().nullable(),
-  title: z.string().nullable(),
-  company: z.string().nullable(),
-  goalDays: z.number(),
-  email: z.string().nullable(),
-  phone: z.string().nullable(),
-  links: z.array(z.string()),
-  tags: z.array(z.string()),
-  linkedInUrl: z.string().nullable(),
-  isArchived: z.boolean(),
-});
+import { generateHeaders } from "~/utils/generateHeaders";
 
 const EXPO_PUBLIC_API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
 
@@ -25,25 +11,13 @@ if (!EXPO_PUBLIC_API_BASE_URL)
 export const useContact = (id: string) => {
   const { getToken } = useAuth();
 
-  async function headers() {
-    const headers = new Map<string, string>();
-
-    const token = await getToken();
-
-    if (token) headers.set("Authorization", token);
-
-    headers.set("Accept", "application/json");
-
-    return Object.fromEntries(headers);
-  }
-
   return useQuery({
     queryKey: ["contact", id],
     queryFn: async () => {
       const response = await fetch(
         `${EXPO_PUBLIC_API_BASE_URL}/api/contacts/${id}`,
         {
-          headers: await headers(),
+          headers: await generateHeaders(getToken),
         }
       );
 
